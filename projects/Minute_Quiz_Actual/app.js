6c7da72b8bcf98070510b3cb8b0bd6e09c0065ff
William Winston <winston.william@gmail.com>
2020-02-09 18:23:01 -0500

    Game rules modal created and tested

--- projects/Minute_Quiz_Actual/app.js
+++ projects/Minute_Quiz_Actual/app.js
@@ -1,6 +1,11 @@
 // console.log("hello world");
 
+
 $(() => {
+                    //////////////////////
+                    /// API connected ////
+                    //////////////////////
+
     // $.ajax({
     //     url: 'https://opentdb.com/api.php?amount=30',
     //     success: (data) => {
@@ -10,17 +15,37 @@ $(() => {
     //         console.log('bad request');
     //     }
 
-    /// variables list ////
+                    //////////////////////////
+                    /// Variables for Game ///
+                    //////////////////////////
+
     //     const $body = $('body')
     //     let remaining = 60;
     //     let timerGo = setInterval(() => {
     //
     //     })
     // })
-    
+
+
+
  ///// Variable for message when Time has elapsed ////
-    let timeUp = function() {
-        alert('Time\'s Up!!!')
+    // let timeUp = function() {
+    //     alert('Time\'s Up!!!')
+    // }
+    // setTimeout(timeUp, 10000)
+
+//// Initial Modal with Game Rules ////
+    const $openBtn = $('#openModal');
+    const $modal = $('#modal');
+    const $closeBtn = $('#close');
+
+    const openModal = () => {
+        $modal.css('display', 'block');
     }
-    setTimeout(timeUp, 10000)
+    const closeModal = () => {
+        $modal.css('display','none');
+    }
+
+    $openBtn.on('click', openModal);
+    $closeBtn.on('click', closeModal);
 });
