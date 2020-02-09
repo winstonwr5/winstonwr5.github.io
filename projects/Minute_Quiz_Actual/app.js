164abb98018cc3115d9c2ebd1ef13030a7b6e561
William Winston <winston.william@gmail.com>
2020-02-08 20:00:49 -0500

    Added variable for time elapsed message

--- projects/Minute_Quiz_Actual/app.js
+++ projects/Minute_Quiz_Actual/app.js
@@ -1,13 +1,25 @@
 // console.log("hello world");
 
 $(() => {
-    $.ajax({
-        url: 'https://opentdb.com/api.php?amount=10',
-        success: (data) => {
-            console.log(data);
-        },
-        error: ()=>{
-            console.log('bad request');
-        }
-    })
+    // $.ajax({
+    //     url: 'https://opentdb.com/api.php?amount=30',
+    //     success: (data) => {
+    //         console.log(data);
+    //     },
+    //     error: ()=>{
+    //         console.log('bad request');
+    //     }
+
+    /// variables list ////
+    //     const $body = $('body')
+    //     let remaining = 60;
+    //     let timerGo = setInterval(() => {
+    //
+    //     })
+    // })
+ ///// Variable for message when Time has elapsed ////
+    let timeUp = function() {
+        alert('Time\'s Up!!!')
+    }
+    setTimeout(timeUp, 10000)
 });
