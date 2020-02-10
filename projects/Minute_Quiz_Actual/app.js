55454b3ed67ddcc081752fd5eccafa81c8bbd1b4
William Winston <winston.william@gmail.com>
2020-02-09 20:06:48 -0500

    created countdown clock function

--- projects/Minute_Quiz_Actual/app.js
+++ projects/Minute_Quiz_Actual/app.js
@@ -26,26 +26,48 @@ $(() => {
     //     })
     // })
 
+    /////////////////////////////////
+    // function for game countdown //
+    /////////////////////////////////
+
+    // const $secondsTimer = $('#seconds-timer');
+    // let secondsTimer = 60
+    //
+    // let gameTimer = setInterval(function() {
+    //     secondsTimer--;
+    //     if (secondsTimer <= 0) {
+    //         clearInterval(gameTimer);
+    //         $('#game-timer').html('<h2>Time!<h2>');
+    //         return;
+    //     } else {
+    //         $('#countdown').text(secondsTimer);
+    //         console.log("Game-timer -->" + secondsTimer);
+    //     }
+    // }, 1000);
 
 
+});
  ///// Variable for message when Time has elapsed ////
     // let timeUp = function() {
     //     alert('Time\'s Up!!!')
     // }
     // setTimeout(timeUp, 10000)
 
-//// Initial Modal with Game Rules ////
-    const $openBtn = $('#openModal');
-    const $modal = $('#modal');
-    const $closeBtn = $('#close');
+            ///////////////////////////////////////
+            //// Opening Modal with Game Rules ////
+            ///////////////////////////////////////
 
-    const openModal = () => {
-        $modal.css('display', 'block');
-    }
-    const closeModal = () => {
-        $modal.css('display','none');
-    }
-
-    $openBtn.on('click', openModal);
-    $closeBtn.on('click', closeModal);
-});
+//     const $openBtn = $('#openModal');
+//     const $modal = $('#modal');
+//     const $closeBtn = $('#close');
+//
+//     const openModal = () => {
+//         $modal.css('display', 'block');
+//     }
+//     const closeModal = () => {
+//         $modal.css('display','none');
+//     }
+//
+//     $openBtn.on('click', openModal);
+//     $closeBtn.on('click', closeModal);
+// });
