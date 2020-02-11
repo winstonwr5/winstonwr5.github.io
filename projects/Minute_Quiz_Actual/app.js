916f3a7c6eb7c43a3865c9432575475f989c73c5
William Winston <winston.william@gmail.com>
2020-02-11 16:15:01 -0500

    synced up start button and countdown clock

--- projects/Minute_Quiz_Actual/app.js
+++ projects/Minute_Quiz_Actual/app.js
@@ -50,189 +50,297 @@
         //         const userInput = $('input[type="text"]').val()
         //         let startButton = $(event.target).attr('id')
 $(( ) => {
+//             let numberRight = []
+//
+//             let numberWrong = []
+//
+//
+//
+//                     /////////////////////////////////
+//                     /// Function to retrieve data ///
+//                     /////////////////////////////////
+//             function getQuestion() {
+//                 $.ajax({
+//                     url: 'https://opentdb.com/api.php?amount=1',
+//                     success: (data) => {
+//
+//
+//                     // + buttonClick
+//                     // }).then((data) => {
+//
+//                 /// for loop to run through data ////
+//
+//                         // for (let i = 0; i < data.length; i++) {
+//                         //     const $data = $('<dd>').attr('class', 'data')
+//                         //     $('.questions').append($data)
+//                         //     $readout.html("Category :" + data[i].readout)
+//
+//             /// OR putting specific data right on page /////
+//                         console.log(data.results[0]);
+//                         $('#category').html(data.results[0].category)
+//                         $('#type').html(data.results[0].type)
+//                         $('#difficulty').html(data.results[0].difficulty)
+//                         $('#question').html(data.results[0].question)
+//                         $('#correctAnswer').html(data.results[0].correct_answer)
+//                     },
+//                     error: () => {
+//                         console.log('no no no');
+//                     }
+//                 });
+//             }
+//
+//             const startGame = $('#start-button').on('click', (event) => {
+//                 event.preventDefault()
+//                 getQuestion();
+//             ;
+//         });
+//         ;})
+//
+//             const submitAnswer = $('.submitAnswer').on('click', (event) => {
+//                 event.preventDefault()
+//                 let userInputLowerCase = $('input[type="text"]').val().toLowerCase()
+//                 let answerLowerCase = correctAnswer.toLowerCase()
+//
+//                 if (userInputLowerCase === answerLowerCase) {
+//                     console.log('Correct!')
+//                     numberRight.push(rightAnswer)
+//                     $('#correctAnswer').html(data.results[0].correct_answer)
+//                     getQuestion()
+//                     console.log(correctAnswer)
+//                     $('input[type="text"]').val('')
+//                 } else {
+//                     console.log('Incorrect!')
+//                     numberWrong.push(wrongAnswer)
+//                     $('#correctAnswer').html(data.results[0].correct_answer)
+//                     getQuestion()
+//                     $('input[type="text"]').val('')
+//                 }
+//             });
+//
+//             const buttonClick = $('.button').on('click', (event) => {
+//                     event.preventDefault()
+//                     //
+//         //         // console.log(userInput)
+//         //         // console.log(startButton)
+//         //         // let start = () => {
+//         //         // let link = 'https://opentdb.com/api.php?amount=1'
+//
+//
+//     ///////////////////////////////////////////////////////
+//     //// Make answers more forgiving - Doesn't work yet ///
+//     ///////////////////////////////////////////////////////
+//     //
+//     //     const formatResponse = (userInput) => {
+//     //         return userInput[0].toLowerCase()
+//     //     }
+//     //
+//     //     const greet = () => {
+//     //         alert('Hi!')
+//     //     }
+//     //
+//     //     const depart = () => {
+//     //         alert('Bye!')
+//     //     }
+//     //
+//     //     let answer = prompt('Are you (g)oing or (s)taying', 'Going or Staying?')
+//     //
+//     //     let softAnswer = formatResponse(userInput)
+//     //
+//     //     if (softAnswer === 'g') {
+//     //         greet()
+//     //     } else if (softAnswer === 's') {
+//     //         depart()
+//     //     }
+//     // })
+//             /////////////////////////////////////////
+//             ////////// Variables for Game ///////////
+//             /////////////////////////////////////////
+//     // let userInput =
+//     // let startButton = $(event.target).attr('id')
+//     //
+//
+//                     //////////////////////
+//                     /// API connected ////
+//                     //////////////////////
+//
+//     // $.ajax({
+//     //     url: 'https://opentdb.com/api.php?amount=1',
+//     //     success: (data) => {
+//     //         console.log(data);
+//     //     },
+//     //     error: ()=>{
+//     //         console.log('bad request');
+//     //     }
+//     // });
+//
+//
+//         // }
+//         // })
+//
+//                     //////////////////////////
+//                     /// Variables for Game ///
+//                     //////////////////////////
+//
+//     //     const $body = $('body')
+//     //     let remaining = 60;
+//     //     let timerGo = setInterval(() => {
+//     //
+//     //     })
+//     // })
+//
+//                 /////////////////////////////////
+//                 // function for game countdown //
+//                 /////////////////////////////////
+//
+//     const $secondsTimer = $('#seconds-timer');
+//     let secondsTimer = 60
+//
+//     let gameTimer = setInterval(function() {
+//         secondsTimer--;
+//         if (secondsTimer <= 0) {
+//             clearInterval(gameTimer);
+//             $('#game-timer').html('<h2>Time!<h2>');
+//             return;
+//         } else {
+//             $('#countdown').text(secondsTimer);
+//             console.log("Game-timer -->" + secondsTimer);
+//         }
+//     }, 1000);
+//
+//
+// // });
+//         /////////////////////////////////////////////////////
+//         ///// Variable for message when Time has elapsed ////
+//         /////////////////////////////////////////////////////
+//
+//     // let timeUp = function() {
+//     //     alert('Time\'s Up!!!')
+//     // }
+//     // setTimeout(timeUp, 10000)
+//
+//             ///////////////////////////////////////
+//             //// Opening Modal with Game Rules ////
+//             ///////////////////////////////////////
+//
+//     const $openBtn = $('#openModal');
+//     const $modal = $('#modal');
+//     const $closeBtn = $('#close');
+//     const $endModal = $('#game-end-modal');
+//
+//
+//     const openModal = () => {
+//         $modal.css('display', 'block');
+//     }
+//     const endModal = () => {
+//         $modal.css('display', 'block');
+//     }
+//     const closeModal = () => {
+//         $modal.css('display', 'none');
+//     }
+//     $openBtn.on('click', openModal);
+//     $closeBtn.on('click', closeModal);
+$(( ) => {
+    let numberRight = []
+
+    let numberWrong = []
+
+    const startButton = $('#start-button').on('click', (event) => {
+        event.preventDefault()
+        gameTimer()
+        getQuestion()
+    })
+    let correctAnswer
+
+    function getQuestion() {
+        $.ajax({
+            url: 'https://opentdb.com/api.php?amount=1',
+            success: (data) => {
+                console.log(data.results[0]);
+                $("#category").html(data.results[0].category)
+                $("#type").html(data.results[0].type)
+                $("#difficulty").html(data.results[0].difficulty)
+                $("#question").html(data.results[0].question)
+                correctAnswer = data.results[0].correct_answer;
+                // handleAnswer();
+                },
+                error: ()=>{
+                console.log('bad request');
+                }
+            });
+
+    function handleAnswer(answer) {
+
+                }
+        }
+
                     /////////////////////////////////
-                    /// Function to retrieve data ///
+                    // function for game countdown //
                     /////////////////////////////////
-            function getQuestion() {
-                $.ajax({
-                    url: 'https://opentdb.com/api.php?amount=1',
-                    success: (data) => {
 
+    const $secondsTimer = $('#seconds-timer');
+    let secondsTimer = 60
 
-                    // + buttonClick
-                    // }).then((data) => {
+    function gameTimer() {
+        setInterval(() => {
+        secondsTimer--;
+        if (secondsTimer <= 0) {
+            clearInterval(gameTimer);
+            $('#game-timer').html('<h2>Time!<h2>');
+            return;
+        } else {
+            $('#countdown').text(secondsTimer + " secs");
+            console.log("Game-timer -->" + secondsTimer);
+        }
+    }, 1000);
+};
 
-                /// for loop to run through data ////
-
-                        // for (let i = 0; i < data.length; i++) {
-                        //     const $data = $('<dd>').attr('class', 'data')
-                        //     $('.questions').append($data)
-                        //     $readout.html("Category :" + data[i].readout)
-
-            /// OR putting specific data right on page /////
-                        console.log(data.results[0]);
-                        $('#category').html(data.results[0].category)
-                        $('#type').html(data.results[0].type)
-                        $('#difficulty').html(data.results[0].difficulty)
-                        $('#question').html(data.results[0].question)
-                        // $('#correctAnswer').html(data.results[0].correct_answer);
-                    },
-                    error: () => {
-                        console.log('no no no');
-                    }
-                });
-            }
-
-            const startGame = $('#start-button').on('click', (event) => {
-                event.preventDefault()
-                getQuestion();
-            ;
+        const closeModal = $('.modal-button').on('click', (event) => {
+            event.preventDefault()
+            getQuestion()
         });
-        ;})
 
-            const submitAnswer = $('.submit-answer').on('click', (event) => {
-                event.preventDefault()
-                let userInputLowerCase = $('input[type="text"]').val().toLowerCase()
-                let answerLowerCase = correctAnswer.toLowerCase()
+        const submitAnswer = $('.submit-answer').on('click', (event) => {
+            event.preventDefault()
+            let userInputLowerCase = $('input[type="text"]').val().toLowerCase()
+            let answerLowerCase = correctAnswer.toLowerCase()
 
-                if (userInputLowerCase === answerLowerCase) {
-                    console.log('Correct!');
-                    // numberRight.push(rightAnswer)
-                    $('#correctAnswer').html(data.results[0].correct_answer)
-                    getQuestion()
-                    console.log(correctAnswer)
-                    $('input[type="text"]').val('')
+            if (userInputLowerCase === answerLowerCase) {
+                console.log('right!')
+                // numberRight.push(rightAnswer)
+                getQuestion()
+                console.log(correctAnswer)
+                $('input[type="text"]').val('')
                 } else {
-                    console.log('Incorrect!')
+                    console.log('wrong')
                     // numberWrong.push(wrongAnswer)
-                    $('#correctAnswer').html(data.results[0].correct_answer)
                     getQuestion()
                     $('input[type="text"]').val('')
                 }
             });
 
-            const buttonClick = $('.button').on('click', (event) => {
-                    event.preventDefault()
-                    //
-        //         // console.log(userInput)
-        //         // console.log(startButton)
-        //         // let start = () => {
-        //         // let link = 'https://opentdb.com/api.php?amount=1'
+        const buttonClick = $('.button').on('click', (event) => {
+            event.preventDefault()
+            //
+            const userInput = $('input[type="text"]').val()
 
 
-    ///////////////////////////////////////////////////////
-    //// Make answers more forgiving - Doesn't work yet ///
-    ///////////////////////////////////////////////////////
-    //
-    //     const formatResponse = (userInput) => {
-    //         return userInput[0].toLowerCase()
-    //     }
-    //
-    //     const greet = () => {
-    //         alert('Hi!')
-    //     }
-    //
-    //     const depart = () => {
-    //         alert('Bye!')
-    //     }
-    //
-    //     let answer = prompt('Are you (g)oing or (s)taying', 'Going or Staying?')
-    //
-    //     let softAnswer = formatResponse(userInput)
-    //
-    //     if (softAnswer === 'g') {
-    //         greet()
-    //     } else if (softAnswer === 's') {
-    //         depart()
-    //     }
-    // })
-            /////////////////////////////////////////
-            ////////// Variables for Game ///////////
-            /////////////////////////////////////////
-    // let userInput =
-    // let startButton = $(event.target).attr('id')
-    //
+                    ///////////////////////////////////////
+                  //// Opening Modal with Game Rules ////
+                  ///////////////////////////////////////
+        const $openBtn = $('#openModal');
+        const $modal = $('#modal');
+        const $closeBtn = $('#close');
 
-                    //////////////////////
-                    /// API connected ////
-                    //////////////////////
-
-    // $.ajax({
-    //     url: 'https://opentdb.com/api.php?amount=1',
-    //     success: (data) => {
-    //         console.log(data);
-    //     },
-    //     error: ()=>{
-    //         console.log('bad request');
-    //     }
-    // });
-
-
-        // }
-        // })
-
-                    //////////////////////////
-                    /// Variables for Game ///
-                    //////////////////////////
-
-    //     const $body = $('body')
-    //     let remaining = 60;
-    //     let timerGo = setInterval(() => {
-    //
-    //     })
-    // })
-
-                /////////////////////////////////
-                // function for game countdown //
-                /////////////////////////////////
-
-    const $secondsTimer = $('#seconds-timer');
-    let secondsTimer = 60
-
-    let gameTimer = setInterval(function() {
-        secondsTimer--;
-        if (secondsTimer <= 0) {
-            clearInterval(gameTimer);
-            $('#game-timer').html('<h2>Time!<h2>');
-
-
-            return;
-        } else {
-            $('#countdown').text(secondsTimer);
-            console.log("Game-timer -->" + secondsTimer);
+        const openModal = () => {
+        $modal.css('display', 'block');
         }
-    }, 1000);
 
-
-// });
-        /////////////////////////////////////////////////////
-        ///// Variable for message when Time has elapsed ////
-        /////////////////////////////////////////////////////
-
-    // let timeUp = function() {
-    //     alert('Time\'s Up!!!')
-    // }
-    // setTimeout(timeUp, 10000)
-
-            ///////////////////////////////////////
-            //// Opening Modal with Game Rules ////
-            ///////////////////////////////////////
-
-    const $openBtn = $('#openModal');
-    const $modal = $('#modal');
-    const $closeBtn = $('#close');
-    const $endModal = $('game-end-modal')
-
-
-    const openModal = () => {
-        $modal.css('display', 'block');
-    }
-    const endModal = () => {
-        $modal.css('display', 'block');
-    }
-    const closeModal = () => {
+        const closeModal = () => {
         $modal.css('display', 'none');
-    }
-    $openBtn.on('click', openModal);
-    $closeBtn.on('click', closeModal);
+        }
+
+        $openBtn.on('click', openModal);
+        $closeBtn.on('click', closeModal);
+        });
+    });
 });
